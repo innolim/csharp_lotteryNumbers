5e242c4e93bc9c98818e0ad0bd9a11a3e63bddab
innolim <128082075+innolim@users.noreply.github.com>
2023-06-27 21:01:23 -0400

    Add files via upload

---
+++ lotteryNumbers/lotteryNumbers/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace lotteryNumbers
+{
+    partial class Lottery
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstLabel = new System.Windows.Forms.Label();
+            this.secondLabel = new System.Windows.Forms.Label();
+            this.thirdLabel = new System.Windows.Forms.Label();
+            this.fourthLabel = new System.Windows.Forms.Label();
+            this.fifthLabel = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstLabel
+            // 
+            this.firstLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.firstLabel.Location = new System.Drawing.Point(62, 65);
+            this.firstLabel.Name = "firstLabel";
+            this.firstLabel.Size = new System.Drawing.Size(100, 46);
+            this.firstLabel.TabIndex = 0;
+            // 
+            // secondLabel
+            // 
+            this.secondLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.secondLabel.Location = new System.Drawing.Point(193, 66);
+            this.secondLabel.Name = "secondLabel";
+            this.secondLabel.Size = new System.Drawing.Size(100, 45);
+            this.secondLabel.TabIndex = 1;
+            // 
+            // thirdLabel
+            // 
+            this.thirdLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.thirdLabel.Location = new System.Drawing.Point(317, 66);
+            this.thirdLabel.Name = "thirdLabel";
+            this.thirdLabel.Size = new System.Drawing.Size(100, 45);
+            this.thirdLabel.TabIndex = 2;
+            // 
+            // fourthLabel
+            // 
+            this.fourthLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fourthLabel.Location = new System.Drawing.Point(461, 65);
+            this.fourthLabel.Name = "fourthLabel";
+            this.fourthLabel.Size = new System.Drawing.Size(100, 46);
+            this.fourthLabel.TabIndex = 3;
+            // 
+            // fifthLabel
+            // 
+            this.fifthLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fifthLabel.Location = new System.Drawing.Point(613, 65);
+            this.fifthLabel.Name = "fifthLabel";
+            this.fifthLabel.Size = new System.Drawing.Size(100, 46);
+            this.fifthLabel.TabIndex = 4;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.generateButton.Location = new System.Drawing.Point(237, 231);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(146, 63);
+            this.generateButton.TabIndex = 5;
+            this.generateButton.Text = "Generate Numbers";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.exitButton.Location = new System.Drawing.Point(409, 231);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(137, 63);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Lottery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.fifthLabel);
+            this.Controls.Add(this.fourthLabel);
+            this.Controls.Add(this.thirdLabel);
+            this.Controls.Add(this.secondLabel);
+            this.Controls.Add(this.firstLabel);
+            this.Name = "Lottery";
+            this.Text = "Lottery Numbers";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstLabel;
+        private System.Windows.Forms.Label secondLabel;
+        private System.Windows.Forms.Label thirdLabel;
+        private System.Windows.Forms.Label fourthLabel;
+        private System.Windows.Forms.Label fifthLabel;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ lotteryNumbers/lotteryNumbers/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lotteryNumbers
+{
+    public partial class Lottery : Form
+    {
+        public Lottery()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            // create an array to hold the numbers
+            const int SIZE = 5;
+            int[] lotteryNumbers = new int[SIZE];
+
+            // create a random object
+            Random rand = new Random();
+
+            // fill the array with random numbers int he range of 0-99
+            for (int index = 0; index < lotteryNumbers.Length; index++)
+            {
+                lotteryNumbers[index] = rand.Next(100);
+            }
+
+            // display the array elements in the Label controls
+            firstLabel.Text = lotteryNumbers[0].ToString();
+            secondLabel.Text = lotteryNumbers[1].ToString();
+            thirdLabel.Text = lotteryNumbers[2].ToString();
+            fourthLabel.Text = lotteryNumbers[3].ToString();
+            fifthLabel.Text = lotteryNumbers[4].ToString();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
